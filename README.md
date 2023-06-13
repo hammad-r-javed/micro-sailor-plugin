387f6188131e8037c7908e130df8eb4cfdf1393f
hammad-r-javed <hammad.javed2002@gmail.com>
2023-06-13 01:33:24 +0100

    show declaration command implemented

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # Sailor
-Sailor is a code navigation plugin for the Micro editor. It uses ctags
+Sailor is a code navigation plugin for the Micro editor.
 
 ## Installation
+### Requirements
+* [universal-ctags](https://github.com/universal-ctags/ctags)
+* tag files using the universal-ctags extended format
+
+### Instructions
 Install `micro-sailor-plugin` by simply cloning this repo into your `~/.config/micro/plug` directory
 
-## Features
+## Configuration
+Sailor is best used when working inside a project rather than when reading and editing random files. Sailor creates `.sail` files which it stores ctag generated tags in. 
+
+Add `*.sail` to your gitignore. 
+
+## Features / How to use
 Main Focuses:
-* Press `F12` to jump to function definition
+* Press `Ctrl-p` to jump to function definition
 * Display function declaration of function being hovered over or selected
 
 Hope to Delvier at some point:
@@ -14,3 +24,8 @@ Hope to Delvier at some point:
     * Create jump points in any file
     * Be able to quickly switch between jump points
 * Provide auto-complete options for things like object methods
+* View instances where function was called, and jump to said instance
+
+## Dev Todo
+* Redesign so that effects are pushed to edges of plugin
+* Have half-decent error handling
--- sailor.lua
+++ sailor.lua
@@ -1,12 +1,123 @@
 VERSION = "1.0.0"
 
+local lua_io = require("io")
+local lua_os = require("os")
+
+local micro_os = import("os")
 local micro = import("micro")
 local config = import("micro/config")
+local shell = import("micro/shell")
+
+local allTags = {}
+
+-- INIT / CONFIG --
 
 function init()
-    config.MakeCommand("gotodef", gotoDefinition, config.NoComplete)
+    config.MakeCommand("gotodef", gotoDef, config.NoComplete)
+    config.MakeCommand("showdec", showDec, config.NoComplete)
+
+    tagsLoaded = loadTags()
+    if not tagsLoaded then
+        micro.InfoBar():Message("Sailor - Tags file could not be parsed correctly!")
+    end
+
+    -- TODO - come up with sname keybinds
+    config.TryBindKey("Ctrl-p", "command:showdec", true)
+    -- config.TryBindKey("F9", "command:gotodef", true)
 end
 
-function gotoDefinition(bp, args)
-    micro.InfoBar():Message("gotoDefinition called!") -- TODO - FOR DEBUGGING
+-- TODO - handle this better, maybe only do this when opened outside of a project  
+function preQuit()
+    lua_os.execute("rm *.sail")
+end
+
+
+-- NAVIGATION --
+
+-- TODO - complete function implementation
+function gotoDef(bp, args)
+    micro.InfoBar():Message("'gotoDefinition' Not implemented yet!") -- TODO - Implement function
+end
+
+-- TODO - finish implementing function
+function getDef()
+end
+
+function showDec(bp, args)
+    local selection = getSelectedText()
+    local dec = getDec(selection)
+
+    if dec == nil then
+        micro.InfoBar():Message("Could not find declaration for '" .. selection .. "'")
+    else
+        micro.InfoBar():Message(dec)
+    end
+end
+
+-- CTAGS INTERACTION --
+
+function getDec(text)
+    for index,tag in ipairs(allTags) do
+        if tag["name"] == text then return tag["declaration"] end
+    end
+
+    return nil
+end
+
+function loadTags()
+    local fileType = micro.CurPane().Buf:FileType()
+    
+    local tagFileName = "sailor-" .. fileType .. "-tagfile.sail"
+    if not fileExists(tagFileName) then
+        micro.InfoBar():Message("Sailor - Tags Generated")
+        lua_os.execute("ctags -R --languages=" .. fileType .. " -f " .. tagFileName)
+    end
+
+    local result = parseTags(tagFileName)
+    return result
+end
+
+-- TODO - handle fil IO and invalid data errors
+function parseTags(tagFileName)
+    local next = next
+    local tagFile = lua_io.open(tagFileName, "r")
+    lua_io.input(tagFile)
+
+    for line in lua_io.lines() do
+        local splitLine = {}
+        for word in string.gmatch(line, "([^	]+)") do
+            table.insert(splitLine, word)
+        end
+        if next(splitLine) ~= nil then
+            table.insert(allTags, {
+                name=splitLine[1],
+                file=splitLine[2],
+                declaration=splitLine[3],
+                type=splitLine[4]
+            })
+        end
+    end
+
+    return true
+end
+
+
+-- UTILS --
+
+function getSelectedText()
+    local curPane = micro.CurPane()
+    local cursor = curPane.Cursor
+    
+    if not cursor:HasSelection() then
+        micro.CurPane().Cursor:SelectWord()
+    end
+
+    local line = curPane.Buf:Line(cursor.CurSelection[1].Y)
+    return string.sub(line, cursor.CurSelection[1].X+1, cursor.CurSelection[2].X)
+end
+
+function fileExists(path)
+    local result, err = micro_os.Stat(path)
+    if err == nil then return true end
+    return false
 end
